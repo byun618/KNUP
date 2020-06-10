25bad99acd73cfcd33e489966e1b90884a3ce3fd
yuminee <33755241+yuminee@users.noreply.github.com>
2020-06-10 20:19:44 +0900

    dasi

--- KNUP_Server/app/api/file/index.js
+++ KNUP_Server/app/api/file/index.js
@@ -4,32 +4,38 @@ const multer = require('multer');
 const mkdirp = require('mkdirp');
 
 
-//사용자인증코드생성
-var vaildcode = Math.floor(Math.random() * 1000000)+100000;
 
-if(vaildcode>1000000){
-    vaildcode = vaildcode - 100000;
-}
 
-var vaildfolder = 'uploads/' + vaildcode
-//인증코드폴더생성
-mkdirp(vaildfolder, function (err) {
-    if(err);
-    console.error(err);
-});
+
+
 
 
 var file = require('./file');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      
+      //사용자인증코드생성
+      var vaildcode = Math.floor(Math.random() * 1000000)+100000;
+
+      if(vaildcode>1000000){
+      vaildcode = vaildcode - 100000;
+}
+
+      var vaildfolder = 'uploads/' + vaildcode;
+      //인증코드폴더생성
+      mkdirp(vaildfolder, function (err) {
+      if(err);
+      console.error(err);
+      });
+
       cb(null, vaildfolder);
      
       
     },
     filename: function (req, file, cb) {
        
-        console.log(result);
+        console.log(file.originalname);
         cb(null, file.originalname); }  
     }
   )
