b0311208c191be48a882ae6012ffe1f25845a0db
yuminee <ayuminee2@gmail.com>
2020-10-14 17:13:32 +0900

    [add] /KNUP/kakao is the main

--- KNUP_Server/app/views/index.js
+++ KNUP_Server/app/views/index.js
@@ -5,7 +5,7 @@ module.exports = router;
 
 /* 로드하고자 하는 페이지 URL 설정 및 rendring */
 router.get('/', (req, res) => res.render('index')); //home
-
+router.get('/kakao', (req,res) => res.render('kakao_main')); //kakaomain
 
 // router.get('/print_submit',(req,res) => res.render('print_submit')); //프린트신청
 // router.get('/print',(req,res) => res.render('print')); //프린트
