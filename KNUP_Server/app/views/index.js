6b46cd5eed2447c1f80074039d34f495b583f67f
myungsworld <myungsworld@gmail.com>
2020-10-18 18:03:17 +0900

    [MOD] key Update

--- KNUP_Server/app/views/index.js
+++ KNUP_Server/app/views/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const { REST_API_KEY } = process.env;
 module.exports = router;
 
 /* 로드하고자 하는 페이지 URL 설정 및 rendring */
 router.get('/', (req, res) => res.render('index')); //home
-router.get('/login', (req,res) => res.render('kakao_main')); //kakaomain
+router.get('/login', (req,res) => {
+
+    console.log(REST_API_KEY)
+
+    res.render('kakao_main', {client_id : REST_API_KEY })
+}); //kakaomain
 
 // router.get('/print_submit',(req,res) => res.render('print_submit')); //프린트신청
 // router.get('/print',(req,res) => res.render('print')); //프린트
